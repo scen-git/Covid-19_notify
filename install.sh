bcfaf17f2106f65ffda6410afe13e5eecfe4d458
bijancot <panjidia995@gmail.com>
2020-04-13 12:19:52 +0700

    initialize sh

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ sudo apt update && sudo apt install curl libnotify-bin jq -y
 echo Exec=$HOME/.config/autostart/Covid-19_notify.sh >> src/Covid-19.desktop
 mv src/* ~/.config/autostart/
 chmod +x ~/.config/autostart/Covid-19_notify.sh
+
+#first run inisialisasi shell
+~/.config/autostart/Covid-19_notify.sh &
