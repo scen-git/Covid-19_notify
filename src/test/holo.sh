3e0ecd33c76814adf285664dc2162b73bc889fae
bijancot <panjidia995@gmail.com>
2020-04-17 15:31:43 +0700

    testing shell for xfce

---
+++ src/test/holo.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+interval=6 #in hours
+
+function notify() {
+    notify-send --app-name=Covid-19 --urgency=critical --icon=dialog-information "Corona virus info" "\nTotal Pasien Positif : $positif \nTotal Pasien Sembuh : $sembuh\nTotal Pasien Meninggal : $meninggal\n\nStay at 127.0.0.1 or ::1 \n\n#dirumahaja"
+    # notify-send --app-name=Covid-19 --urgency=critical "Stay at 127.0.0.1 or ::1" "#dirumahaja"
+    
+}
+
+function kde() {
+  array=("positif : $positif" "sembuh : $sembuh" "meninggal : $meninggal")
+  for i in "${array[@]}"
+    do
+      kdialog --title "Corona virus info" --passivepopup "Total $i"
+      sleep 7
+    done
+    sleep 3
+    kdialog --title "Stay at 127.0.0.1 or ::1" --passivepopup  "#dirumahaja"
+}
+
+function osx() {
+  array=("positif : $positif" "sembuh : $sembuh" "meninggal : $meninggal")
+  for i in "${array[@]}"
+    do
+      osascript -e "display notification \"Total $i\" with title \"Corona virus info\""
+      sleep 7
+    done
+    sleep 3
+    osascript -e 'display notification "#dirumahaja" with title "Stay at 127.0.0.1 or ::1"'
+}
+
+while true
+do
+  api="https://api.kawalcorona.com/indonesia/"
+  data=$(curl -s $api)
+  positif=$(echo $data | jq -r '.[].positif')
+  sembuh=$(echo $data | jq -r '.[].sembuh')
+  meninggal=$(echo $data | jq -r '.[].meninggal')
+  if [[ $(pgrep plasmashell | head -n 1) -gt 1 ]]; then
+    kde
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    osx
+  else
+    notify
+  fi
+
+  sleep $(expr $interval \* 3600)
+done
+
+#Just_Humanz
---
+++ src/test/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+add() {
+    result=$(($1 + $2))
+    echo "Result is: $result"
+}
+
+add 1 2
